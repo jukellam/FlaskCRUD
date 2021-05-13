86a630b0de746a4d4ffd05c1b721d1d3d3e808d3
jukellam <justin@wintermindgroup.com>
2021-05-13 15:43:11 -0600

    end of tutorial

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 # to get venv going - Ctl Shift P, python sel, select venv
 
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy  # add
-from datetime import datetime  # add
+from flask import Flask, render_template, request, redirect 
+from flask_sqlalchemy import SQLAlchemy  
+from datetime import datetime  
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # add
-db = SQLAlchemy(app)  # add
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)  
 
 
-# add
 class Grocery(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -20,5 +19,50 @@ class Grocery(db.Model):
         return '<Grocery %r>' % self.name
 
 
-@app.route('/')
-# ...P
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_stuff = Grocery(name=name)
+
+        try:
+            db.session.add(new_stuff)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem adding new stuff."
+    
+    else:
+        groceries = Grocery.query.order_by(Grocery.created_at).all()
+        return render_template('index.html', groceries=groceries)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    grocery = Grocery.query.get_or_404(id)
+
+    try:
+        db.session.delete(grocery)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting data."
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    grocery = Grocery.query.get_or_404(id)
+
+    if request.method == 'POST':
+        grocery.name = request.form['name']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem updating data."
+
+    else:
+        title = "Update Data"
+        return render_template('update.html', title=title, grocery=grocery)        
+
+if __name__ == '__main__':
+    app.run(debug=True)
